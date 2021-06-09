722771724ec7b253ae6f4aa9952849f866064dc0
Jaimin2499 <58000318+Jaimin2499@users.noreply.github.com>
2021-06-09 21:20:20 +0530

    Add files via upload

---
+++ Demo.js
@@ -0,0 +1,17 @@
+function img1() {
+    document.getElementById('watch').src = "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/44-alum-blue-sport-deepnavy-6s-cell?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1599811391000";
+
+
+}
+
+function img2() {
+    document.getElementById('watch').src = "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/44-alum-silver-sport-white-6s-nc?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1599811720000";
+
+
+}
+
+function img3() {
+    document.getElementById('watch').src = "https://images-na.ssl-images-amazon.com/images/I/81WqPA2ealL._SL1500_.jpg";
+
+
+}
